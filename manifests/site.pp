b6803b8f9dddf1670667b934d885faa58f365afe
Jon Gold <hello@designedbygold.com>
2013-02-28 12:56:31 +0000

    try commenting out extra modules

--- manifests/site.pp
+++ manifests/site.pp
@@ -89,14 +89,14 @@ node default {
 
 # test other packages
 # include notational_velocity::nvalt
-include dropbox
-include postgres
-include chrome::dev
-include mongod
-include zsh
-include sublime_text_2
-include spotify
-include onepassword
-include macvim
-include iterm2::stable
-include divvy
+#include dropbox
+#include postgres
+#include chrome::dev
+#include mongod
+#include zsh
+#include sublime_text_2
+#include spotify
+#include onepassword
+#include macvim
+#include iterm2::stable
+#include divvy
