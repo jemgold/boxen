89d468277ab9dfb4c38b97fb3872ce1f3719122e
Jon Gold <hello@designedbygold.com>
2013-02-28 12:47:00 +0000

    try moving extra stuff to site.pp again

--- manifests/site.pp
+++ manifests/site.pp
@@ -86,3 +86,17 @@ node default {
     target => $boxen::config::repodir
   }
 }
+
+# test other packages
+# include notational_velocity::nvalt
+include dropbox
+include postgres
+include chrome::dev
+include mongod
+include zsh
+include sublime_text_2
+include spotify
+include onepassword
+include macvim
+include iterm2::stable
+include divvy
