c538221b7c45ed3a72b23614fd06d2f0eec59280
Jon Gold <hello@designedbygold.com>
2013-02-28 12:39:47 +0000

    try moving to personal manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -81,21 +81,6 @@ node default {
     ]:
   }
 
-  # test other packages
-  include notational_velocity::nvalt
-  include dropbox
-  include postgres
-  include chrome::dev
-  include mongod
-  include zsh
-  include sublime_text_2
-  include spotify
-  include onepassword
-  include macvim
-  include iterm2::stable
-  include divvy
-
-
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
     target => $boxen::config::repodir
---
+++ modules/people/manifests/jongd.pp
@@ -0,0 +1,17 @@
+class people:jongd {
+
+  # test other packages
+  include notational_velocity::nvalt
+  include dropbox
+  include postgres
+  include chrome::dev
+  include mongod
+  include zsh
+  include sublime_text_2
+  include spotify
+  include onepassword
+  include macvim
+  include iterm2::stable
+  include divvy
+
+}
