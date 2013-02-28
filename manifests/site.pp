7b57781f8d205b2fba6441208ecec8bdffb24327
Jon Gold <hello@designedbygold.com>
2013-02-28 14:57:02 +0000

    add shit back in

--- manifests/site.pp
+++ manifests/site.pp
@@ -88,15 +88,15 @@ node default {
 }
 
 # test other packages
-# include notational_velocity::nvalt
-#include dropbox
-#include postgres
-#include chrome::dev
-#include mongod
-#include zsh
-#include sublime_text_2
-#include spotify
-#include onepassword
-#include macvim
-#include iterm2::stable
-#include divvy
+include notational_velocity::nvalt
+include dropbox
+include postgres
+include chrome::dev
+include mongod
+include zsh
+include sublime_text_2
+include spotify
+include onepassword
+include macvim
+include iterm2::stable
+include divvy
